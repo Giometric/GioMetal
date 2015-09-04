54466982f3d86ca40e2908b9b33e5c72f4e71296
Giovanni Sabella <gio.giometric@gmail.com>
2015-09-03 21:37:37 -0600

    Initial Commit

---
+++ GioMetal/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  GioMetal
+//
+//  Created by Giovanni Sabella on 9/3/15.
+//  Copyright (c) 2015 Giovanni Sabella. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet weak var window: NSWindow!
+    
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(sender: NSApplication) -> Bool {
+        return true;
+    }
+    
+}
